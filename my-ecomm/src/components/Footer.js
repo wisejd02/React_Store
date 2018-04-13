db8e62eab9295cc94c1d4a26784c41eb829fd2b3
Joshua <Joshua@Admins-MacBook.local>
2018-04-12 23:25:47 -0500

    cleaned up footer

--- my-ecomm/src/components/Footer.js
+++ my-ecomm/src/components/Footer.js
@@ -6,28 +6,13 @@ class Foot extends Component {
     render() {
         return (
             <div className="App">
-                <Footer copyrights="&copy 2018 Copyright Text"
-                    // moreLinks={
-                    //     <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-                    // }
-                    // links={
-                    //     <ul>
-                    //         <li><a href="#!"><img className="ico-sm" alt="facebook" src={require("../images/icons/facebook.png")}/></a></li>
-                    //         <li><a href="#!"><img className="ico-sm" alt="facebook" src={require("../images/icons/pinterest.png")}/></a></li>
-                    //         <li><a href="#!"><img className="ico-sm" alt="facebook" src={require("../images/icons/snapchat.png")}/></a></li>
-                    //         <li><a href="#!"><img className="ico-sm" alt="facebook" src={require("../images/icons/facebook.png")}/></a></li>
-                    //     </ul>
-                    // }
-                    // className='example'
-                    
-                >
-                {/* <h5 className="white-text">Footer Content</h5>
-                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p> */}
+                <Footer copyrights="2018 Copyright Text">
+                
                 <div className="cntrFoot">
-                            <a href="#!"><img className="ico-sm" alt="facebook" src={require("../images/icons/facebook.png")}/></a>
-                            <a href="#!"><img className="ico-sm" alt="facebook" src={require("../images/icons/pinterest.png")}/></a>
-                            <a href="#!"><img className="ico-sm" alt="facebook" src={require("../images/icons/snapchat.png")}/></a>
-                            <a href="#!"><img className="ico-sm" alt="facebook" src={require("../images/icons/facebook.png")}/></a>
+                    <a href="https://www.facebook.com/"><img className="ico-sm" alt="facebook" src={require("../images/icons/facebook.png")}/></a>
+                    <a href="https://www.pinterest.com/"><img className="ico-sm" alt="pinterest" src={require("../images/icons/pinterest.png")}/></a>
+                    <a href="https://www.snapchat.com/"><img className="ico-sm" alt="snpchat" src={require("../images/icons/snapchat.png")}/></a>
+                    <a href="https://twitter.com/"><img className="ico-sm" alt="twitter" src={require("../images/icons/twitter.png")}/></a>
                 </div>
             </Footer>
             </div>
